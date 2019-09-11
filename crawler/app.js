24c370060e10e2d8aef45b47125186a32df472a7
gox <gox.roche@gmail.com>
2019-09-12 00:45:34 +0200

    Change main api route

--- crawler/app.js
+++ crawler/app.js
@@ -1,10 +1,11 @@
 const req = require('./req');
 const db = require('../db');
 const config = require('../config');
+const log = require('../logger');
 
-db.connect(config.mongoUrl, config.database, (err) => {
+db.connect(process.env.MONGODB_ADDON_URI || config.mongoUrl, process.env.MONGODB_ADDON_DB || config.database, (err) => {
     if(err) {
-        console.log('Unable to connect to Mongo ' + config.database);
+        log.error('Unable to connect to Mongo ' + config.database);
         process.exit(1);
     } else {
         req.startRequestBand();
--- routes.js
+++ routes.js
@@ -12,84 +12,81 @@ router.use((req, res, next) => {
     if (token) {
         jwt.verify(token, secret, (err, decoded) => {
             if (err) {
-                return res.status(403).send({ success: false, message: 'Failed to authenticate token.' });    
+                return res.status(403).send({ success: false, message: 'Failed to authenticate token.' });
             } else {
-                req.decoded = decoded;  
+                req.decoded = decoded;
                 next();
             }
         });
     } else {
-        return res.status(403).send({ 
-            success: false, 
-            message: 'No token provided' 
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided'
         });
     }
 });
-    
-router.get('/bands', (req, res) => {
-    if(!req.query.genre){
-        res.status(500).send({"message":"Pass a genre name as query parameter"});
-    } else {
-        let genre = req.query.genre.replace("_", ".").toLowerCase();
-        let params = {};
-        if(req.query.name)
-            params.name = new RegExp(["^",".*", req.query.name.replace("_", " "),".*", "$"].join(""), "i");
-        if(req.query.country)
-            params.country = new RegExp(["^", req.query.country.replace("_", " "), "$"].join(""), "i");
-        if(req.query.location)
-            params.location = new RegExp(["^", req.query.location.replace("_", " "), "$"].join(""), "i");
 
-        let limit = parseInt(req.query.limit, 10);
-        if (isNaN(limit) || limit > maxPageSize) {
-            limit = maxPageSize;
-        } else if (limit < 1) {
-            limit = 1;
-        }
-        let page = parseInt(req.query.page, 10);
-        if (isNaN(page) || page < 1) {
-            page = 1;
-        }
-        
-        db.get().collection(genre).find(params).skip((page - 1) * limit).limit(limit).sort({name : 1}).toArray((err, result) => {
-            if(!err){
-                let resp;
-                if(req.query.name){
-                    let bestMatch = [];
-                    let others = [];
-                    let rg = new RegExp(["^",req.query.name.replace("_", " "),".*", "$"].join(""), "i");
-                    result.forEach(function(b) {
-                        if(b.name.match(rg)){
-                            bestMatch.push(b);
-                        } else {
-                            others.push(b);
-                        }
-                    });
-                    resp = { "data": bestMatch.concat(others)};
-                } 
-                else {
-                    resp = { "data": result};
-                }
-                res.status(200).send(resp);
-            } else {
-                res.status(500).send(err);
-            }
-        });
+router.get('/bands', (req, res) => {
+    let params = {};
+    if (req.query.name)
+        params.name = new RegExp(["^", ".*", req.query.name.replace("_", " "), ".*", "$"].join(""), "i");
+    if (req.query.country)
+        params.country = new RegExp(["^", req.query.country.replace("_", " "), "$"].join(""), "i");
+    if (req.query.location)
+        params.location = new RegExp(["^", req.query.location.replace("_", " "), "$"].join(""), "i");
+    if(req.query.genre)
+        params.genre = req.query.genre.replace("_", ".").toLowerCase();
+
+    let limit = parseInt(req.query.limit, 10);
+    if (isNaN(limit) || limit > maxPageSize) {
+        limit = maxPageSize;
+    } else if (limit < 1) {
+        limit = 1;
     }
+    let page = parseInt(req.query.page, 10);
+    if (isNaN(page) || page < 1) {
+        page = 1;
+    }
+
+    db.get().collection("bands").find(params).skip((page - 1) * limit).limit(limit).sort({ name: 1 }).toArray((err, result) => {
+        if (!err) {
+            let resp;
+            if (req.query.name) {
+                let bestMatch = [];
+                let others = [];
+                let rg = new RegExp(["^", req.query.name.replace("_", " "), ".*", "$"].join(""), "i");
+                result.forEach(function (b) {
+                    if (b.name.match(rg)) {
+                        bestMatch.push(b);
+                    } else {
+                        others.push(b);
+                    }
+                });
+                resp = { "data": bestMatch.concat(others) };
+            }
+            else {
+                resp = { "data": result };
+            }
+            res.status(200).send(resp);
+        } else {
+            res.status(500).send(err);
+        }
+    });
 });
 
 router.get('/countries', (req, res) => {
-    if(!req.query.genre){
-        res.status(500).send({"message":"Pass a genre name as query parameter"});
+    if (!req.query.genre) {
+        res.status(500).send({ "message": "Pass a genre name as query parameter" });
     } else {
         let genre = req.query.genre.replace("_", ".").toLowerCase();
         db.get().collection(genre).distinct(
             "country",
             {}, // query object
-            (function(err, docs){
-                if(err){
+            (function (err, docs) {
+                if (err) {
                     res.status(500).send(err);
                 }
-                if(docs){  
+                if (docs) {
                     res.status(200).send(docs.sort());
                 }
             })
